b4d2c92283acc9371b0fa04e2652993f7fc97db4
Daniel Reynolds <danielreynolds101@gmail.com>
2020-03-24 17:43:16 -0600

    Fixed test #1

--- test/controller.js
+++ test/controller.js
@@ -5,24 +5,13 @@ var controller = require('../src/controller')
 
 
 describe('verifyCorrectMap()', function() {
-    it('should return false if nothing has been changed beforehand, should return true if the correct variavle is inputed', function() {
+    it('should return false if nothing has been changed beforehand, should return true if the correct variable is inputed', function() {
       assert.deepEqual(controller.verifyCorrectMap(1), false);
 
-      controller.map1IsBeingMoved = true;
+      controller.mouseDown(1);
+      controller.initializeMap(1, true);
       assert.deepEqual(controller.verifyCorrectMap(1), true);
-      controller.map1IsBeingMoved = false;
-
-      controller.map2IsBeingMoved = true;
-      assert.deepEqual(controller.verifyCorrectMap(2), true);
-      controller.map2IsBeingMoved = false;
-
-      controller.map3IsBeingMoved = true;
-      assert.deepEqual(controller.verifyCorrectMap(3), true);
-      controller.map3IsBeingMoved = false;
-
-      controller.map4IsBeingMoved = true;
-      assert.deepEqual(controller.verifyCorrectMap(4), true);
-      controller.map4IsBeingMoved = false;
+      controller.mouseUp();
   });
 });
 
