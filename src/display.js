65dacdbb87ab560d03e11622e85ee734a9e9699e
Daniel Reynolds <danielreynolds101@gmail.com>
2020-03-24 17:52:50 -0600

    Fixed accidental change to display.js

--- src/display.js
+++ src/display.js
@@ -164,8 +164,8 @@ function geohash_adjacent(geohash, direction) {
 
 
 module.exports = {
-  verifyCorrectMap: verifyCorrectMap,
-  mouseDown: mouseDown,
-  initializeMap: initializeMap,
-  mouseUp: mouseUp
+    decode_geohash: decode_geohash,
+    encode_geohash: encode_geohash,
+    geohash_bounds: geohash_bounds,
+    geohash_adjacent: geohash_adjacent
 }
