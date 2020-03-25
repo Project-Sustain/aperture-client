447a7fb38f0d65fe3bfcc7feb25db3cbb0766d53
Daniel Reynolds <danielreynolds101@gmail.com>
2020-03-24 19:53:24 -0600

    Experimenting with tests for the OSM map

---
+++ test/Iframe/map2.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+var jsdom = require('jsdom-global')();
+
+var map2 = require('../src/Iframe/map2')
+
+
+describe('mapObjectGetter()', function() {
+    it('should return a layer with id OverPassLayer', function() {
+      assert.deepEqual(map2.mapObjectGetter('drinking_water').options.id, 'OverPassLayer');
+  });
+});
+
+
