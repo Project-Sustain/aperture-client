3377b631f2311a0e46f766d5dd60350567fbf4ee
Matt Young <honeybadgerofdoom@gmail.com>
2021-03-11 17:26:48 -0700

    Organization

--- src/iframe/js/library/charting/chartButtonsSpecific.js
+++ src/iframe/js/library/charting/chartButtonsSpecific.js
@@ -1,59 +1,9 @@
-document.getElementById('nav-graph-button').addEventListener('click', showGraph);
-let buttonsOn = false;
 let maxGraphs = 4;
 let totalGraphs = 0;
 let maxGraphsReached = false;
 
-function showGraph() {
-	chartSystem.toggleVisible();
-	if(!buttonsOn) {
-		createAddChartArea();
-		makeButtonsWork();
-		buttonsOn = true;
-	}
-}
-
-function createAddChartArea() {
-	let addGraphBox = document.createElement("div");
-	addGraphBox.className = "colorMode1 customBorder add-graph-box";
-	addGraphBox.id = "graph-controller";
-	let addGraphMessage = document.createElement("p");
-	addGraphMessage.className = "add-graph-message";
-	addGraphMessage.innerText = "Add a...";
-
-	let graphButtonArea = document.createElement("div");
-	graphButtonArea.id = "graph-button-area"
-	let histogramButton = document.createElement("button");
-	histogramButton.className = "btn btn-outline-dark graph-button";
-	histogramButton.id = "histogram-button-id";
-	histogramButton.innerText = "Histogram";
-	let scatterplotButton = document.createElement("button");
-	scatterplotButton.className = "btn btn-outline-dark graph-button";
-	scatterplotButton.id = "scatterplot-button-id";
-	scatterplotButton.innerText = "Scatterplot";
-	let lineGraphButton = document.createElement("button");
-	lineGraphButton.className = "btn btn-outline-dark graph-button";
-	lineGraphButton.id = "linegraph-button-id";
-	lineGraphButton.innerText = "Line Graph";
-
-	graphButtonArea.appendChild(histogramButton);
-	graphButtonArea.appendChild(scatterplotButton);
-	graphButtonArea.appendChild(lineGraphButton);
-
-	addGraphBox.appendChild(addGraphMessage);
-	addGraphBox.appendChild(graphButtonArea);
-	document.getElementById('box1').appendChild(addGraphBox);
-}
-
-function makeButtonsWork() {
-	document.getElementById('histogram-button-id').addEventListener('click', createHistogram);
-	document.getElementById('scatterplot-button-id').addEventListener('click', createScatterplot);
-	document.getElementById('linegraph-button-id').addEventListener('click', createLinegraph);
-}
-
 function createHistogram() {
 	moreGraphsPossible();
-	// let box1 = document.getElementById("box1");
 
 	let graphBox = document.createElement("div");
 	graphBox.className = "colorMode1 customBorder temp-chart-box";
