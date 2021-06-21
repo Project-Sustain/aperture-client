fedce66960eb67ccb2048cbaacb958f1a1ae979b
Matt Young <honeybadgerofdoom@gmail.com>
2021-06-21 07:59:26 -0600

    switching

--- src/js/ui/Sidebar.js
+++ src/js/ui/Sidebar.js
@@ -1,47 +1,19 @@
 import React, {useState} from 'react';
 import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import TabSystem from "./TabSystem"
 import MenuIcon from '@material-ui/icons/Menu';
 import {Button} from "@material-ui/core";
 
-const drawerWidth = 800;
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex',
-        zIndex: 2001,
-    },
-    menuButton: {
-        margin: theme.spacing(5),
-        zIndex: 2002,
-        top: -20,
-        left: -20,
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-        opacity: 0.95,
-    },
-}));
-
 export default function Sidebar() {
-    const classes = useStyles();
     const [open, setOpen] = useState(false);
 
     return (
-        <div className={classes.root}>
+        <div className='sidebar'>
             <Button
                 variant="outlined"
                 color="inherit"
-                className={clsx(classes.menuButton, open && classes.hide)}
+                className={clsx('menu-button', open && 'hide')}
                 startIcon={<MenuIcon />}
                 aria-label="open drawer"
                 onClick={() => setOpen(true)}
@@ -49,13 +21,11 @@ export default function Sidebar() {
                 Menu
             </Button>
             <Drawer
-                className={classes.drawer}
+                className='drawer'
                 variant="persistent"
                 anchor="left"
                 open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
+                className='drawer-paper'
             >
                 <TabSystem handleDrawerClose={() => {setOpen(false)}}/>
             </Drawer>
